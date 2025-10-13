f9e19753447fe3e6f00b7ace0f0cb8473ff44863
Fayis <fayizpachu217@gmail.com>
2025-10-13 13:28:41 +0530

    create user mail sendgrid almost local ok

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,5 +1,12 @@
 import nodemailer from "nodemailer";
 
+import dotenv from "dotenv";
+
+
+
+
+dotenv.config();
+
 const transporter = nodemailer.createTransport({
   host: "smtp.sendgrid.net",
   port: 587,
